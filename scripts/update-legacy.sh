ac802adebadbaa6ccc35efd4efee5dedba02121e
Raul Quispe (raulingg) <relaxedchild@gmail.com>
2019-10-03 12:40:31 -0500

    fix(scripts): Update destination path

--- scripts/update-legacy.sh
+++ scripts/update-legacy.sh
@@ -8,28 +8,28 @@ tmpdir=`mktemp -d 2>/dev/null || mktemp -d -t 'tmpdir'`
 git worktree add "$tmpdir" master
 
 parts=(
-    01-introduction/00-welcome-and-orientation:01-welcome-and-orientation
-    01-introduction/01-growth-mindset:02-growth-mindset
-    01-introduction/02-why-learn-to-code:03-why-learn-to-code
-    01-introduction/03-your-first-website:04-your-first-website
-    01-introduction/04-quiz:05-quiz
-    02-variables-and-data-types/00-values-data-types-and-operators:06-values-data-types-and-operators
-    02-variables-and-data-types/01-variables:07-variables
-    02-variables-and-data-types/02-self-learning-MDN:08-self-learning-MDN
-    02-variables-and-data-types/03-comments:09-comments
-    02-variables-and-data-types/04-guided-exercises:10-guided-exercises
-    02-variables-and-data-types/05-quiz:11-quiz
-    02-variables-and-data-types/06-exercises:12-exercises
-    03-ux-design/00-development-team:13-development-team
-    03-ux-design/01-ux-design:14-ux-design
-    03-ux-design/02-ux-design-vs-ui-design:15-ux-design-vs-ui-design
-    03-ux-design/03-quiz:16-quiz
+    admission-legacy/01-introduction/00-welcome-and-orientation:01-welcome-and-orientation
+    admission-legacy/01-introduction/01-growth-mindset:02-growth-mindset
+    admission-legacy/01-introduction/02-why-learn-to-code:03-why-learn-to-code
+    admission-legacy/01-introduction/03-your-first-website:04-your-first-website
+    admission-legacy/01-introduction/04-quiz:05-quiz
+    admission-legacy/02-variables-and-data-types/00-values-data-types-and-operators:06-values-data-types-and-operators
+    admission-legacy/02-variables-and-data-types/01-variables:07-variables
+    admission-legacy/02-variables-and-data-types/02-self-learning-MDN:08-self-learning-MDN
+    admission-legacy/02-variables-and-data-types/03-comments:09-comments
+    admission-legacy/02-variables-and-data-types/04-guided-exercises:10-guided-exercises
+    admission-legacy/02-variables-and-data-types/05-quiz:11-quiz
+    admission-legacy/02-variables-and-data-types/06-exercises:12-exercises
+    admission-legacy/03-ux-design/00-development-team:13-development-team
+    admission-legacy/03-ux-design/01-ux-design:14-ux-design
+    admission-legacy/03-ux-design/02-ux-design-vs-ui-design:15-ux-design-vs-ui-design
+    admission-legacy/03-ux-design/03-quiz:16-quiz
 )
 
 for part in ${parts[@]}; do
   pair=(${part//:/ })
   src="${tmpdir}/admission-es/03-prework/${pair[1]}"
-  dest="admission-es/${pair[0]}"
+  dest="${pair[0]}"
   ((total=total+1))
 
   changes=$( diff "${dest}/README.md" "${src}/README.md" )
@@ -37,6 +37,7 @@ for part in ${parts[@]}; do
     ((changed=changed+1))
     echo "${src}/README.md" "->" "${dest}/README.md"
     echo "$changes"
+    cp "${src}/README.md" "${dest}/README.md"
   fi
 done
 
